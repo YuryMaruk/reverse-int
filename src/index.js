b8dc4e7568297c7ce995562ccb2f0e704ae6486e
Yury Maruk <maruk.dev@gmail.com>
2022-06-20 23:56:09 +0300

    refactor: change code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
     n = String(n);
-    return n[0] === '-' ? n = Number(n.substr(1).split('').reverse().join('')) :
+    return n[0] === '-' ? Number(n.substr(1).split('').reverse().join('')) :
         Number(n.split('').reverse().join(''));
 }
