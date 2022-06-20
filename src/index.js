0dc09c2333159ac3d80276c3ea9e5f8cc00efb64
Yury Maruk <maruk.dev@gmail.com>
2022-06-20 23:51:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    return n[0] === '-' ? n = Number(n.substr(1).split('').reverse().join('')) :
+        Number(n.split('').reverse().join(''));
 }
